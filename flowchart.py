85f1953002818156615e7c4b8da71261e6627408
Cagedhawk1 <Mail@tysen.info>
2025-05-05 10:39:32 +1200

    first commit

---
+++ flowchart.py
@@ -0,0 +1,13 @@
+num = input("Your Number: ")
+i = num
+
+while i < 0:
+    if i > 0:
+        i = i - 1
+        print(i)
+        continue
+    else:   
+        print("Blast off")
+        break
+
+    
