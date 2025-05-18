6d5d4120ee8b5844a6de00860fdc41b2e885cb7f
Cagedhawk1 <Mail@tysen.info>
2025-05-19 10:35:56 +1200

    1.13 Session working

--- app.py
+++ app.py
@@ -7,7 +7,12 @@ Session = sessionmaker(bind=engine)
 session = Session()
 
 car = Car(car_name = "Skyline", car_price = "50000", car_year=2019)
+car2 = Car(car_name = "GTR", car_price = "100000", car_year=2020)
+car3 = Car(car_name = "Supra", car_price = "60000", car_year=2021)
 
-session.add(car)   
+# Add a single instance of the Car class to the session
+session.add(car2)   
+# Commits all the session changes (Uncoment line below to add all cars)
+# session.add_all([car])
 
 session.commit()
