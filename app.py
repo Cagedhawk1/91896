956eb49d54a38b3fc5edb3cd8f577f0a66b6066e
Cagedhawk1 <Mail@tysen.info>
2025-05-19 10:38:29 +1200

    1,14 accesing the database

--- app.py
+++ app.py
@@ -11,8 +11,14 @@ car2 = Car(car_name = "GTR", car_price = "100000", car_year=2020)
 car3 = Car(car_name = "Supra", car_price = "60000", car_year=2021)
 
 # Add a single instance of the Car class to the session
-session.add(car2)   
+session.add(car3)   
 # Commits all the session changes (Uncoment line below to add all cars)
 # session.add_all([car])
 
+# Commit the session to save the changes to the database
 session.commit()
+
+# Access car data
+cars = session.query(Car).all()
+
+print(car)
