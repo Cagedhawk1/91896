600f0d7100c86fce2c68158c3260b5cd31f4fb3f
Cagedhawk1 <Mail@tysen.info>
2025-05-19 10:11:09 +1200

    1.09 First table work

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.orm import declarative_base
 
 
@@ -7,5 +7,12 @@ db_url = "sqlite:///database.db"
 engine = create_engine(db_url)
 
 Base = declarative_base()
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    age = Column(Integer)
 
 Base.metadata.create_all(engine)
