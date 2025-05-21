8a016538c3ebcabfba67910e10a079507efe6255
Cagedhawk1 <Mail@tysen.info>
2025-05-21 12:25:14 +1200

    1.19.1 Routes work

--- routes.py
+++ routes.py
@@ -1,1 +1,8 @@
-hi
+from models import Car_manufacturer, Car_bodystyle, Car_model, Car_stock, car_images
+
+def register_routes(app, db):
+    @app.route('/')
+    def index():
+        # Display the number of manufacturers in the database
+        manufacturers = Car_manufacturer.query.all()
+        return f"Found {len(manufacturers)} manufacturers in the database. <br><a href='/add-sample'>Add sample data</a>"
