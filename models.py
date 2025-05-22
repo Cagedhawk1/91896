0b04d0c0410547411facfc8f3bf0da1183846fac
Cagedhawk1 <Mail@tysen.info>
2025-05-23 09:19:59 +1200

    2.1.1 DATABASE creation

--- models.py
+++ models.py
@@ -12,8 +12,27 @@ class Car(Base):
     __tablename__ = "cars"
 
     id = Column(Integer, primary_key=True)
-    car_name = Column(String)
+    car_model = Column(String)
+    seller_name = Column(String)
+    car_location = Column(String)
+    car_description = Column(String)
+    car_condition = Column(String)
+    car_color = Column(String)
     car_price = Column(Integer)
     car_year = Column(Integer)
+    car_mileage = Column(Integer)
+    car_bodystyle = Column(String)
+    car_manufacturer = Column(String)
+    car_image1 = Column(String)
+    car_image2 = Column(String)
+    car_image3 = Column(String)
+    car_image4 = Column(String)
+    car_image5 = Column(String)
+    car_image6 = Column(String)
+    car_image7 = Column(String)
+    car_image8 = Column(String)
+    car_image9 = Column(String)
+    car_image10 = Column(String)
+
 
 Base.metadata.create_all(engine)
