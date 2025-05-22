c769e8167d1ae79eccf201988f93599c65646807
Cagedhawk1 <Mail@tysen.info>
2025-05-23 09:22:54 +1200

    2.1.2 Database work

--- app.py
+++ app.py
@@ -1,34 +1,26 @@
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import DeclarativeBase, sessionmaker
 
 from models import Car, engine
+
 
 Session = sessionmaker(bind=engine)
 
 session = Session()
 
-car = Car(car_name = "Skyline", car_price = "50000", car_year=2019)
-car2 = Car(car_name = "GTR", car_price = "100000", car_year=2020)
-car3 = Car(car_name = "Supra", car_price = "60000", car_year=2021)
+cars = session.query(Car).all()
 
-# Add a single instance of the Car class to the session (Uncoment line below to add select cars)
-# session.add(car3)   
-# Commits all the session changes (Uncoment line below to add all cars)
-# session.add_all([car])
-
-# Commit the session to save the changes to the database
-session.commit()
-
-# Access car data
-cars = session.query(Car).filter_by(id=1).all()
 
 # Index position
 #cars = cars[0]
 
-#print(cars.id)
-print(cars.car_name)
-#print(cars.car_price)
-#print(cars.car_year)
 
 # Accessing all elements (Everything in the database)
-for car in cars:
-    print(f"Car ID: {car.id}, Car Name: {car.car_name}, Car Price: {car.car_price}, Car Year: {car.car_year}")
+
+# Update a car
+#car_name = "Not Skyline"
+
+# delete a car
+# session.delete(car)
+
+# Commit the session to save the changes to the database
+session.commit()
